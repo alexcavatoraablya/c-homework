4b95c0b041591804ee2599a1aec917e258ebf183
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-09-09 11:59:51 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+Random rnd = new Random(0);
+//Перетворорюємо рядок в int
+int n = int.Parse(Console.ReadLine() ?? "0"); //вертає string - не можна перетворити в int
+int[] marks; //Зберігає оцінки - він пустий
+marks = new int[n]; //виділив память на масив 10 елементів
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"{i} -> ");
+    marks[i] = int.Parse(Console.ReadLine() ?? "0");
+}
+
+int sum = 0;
+foreach (int item in marks)
+{
+    sum += item; //Зберігаємо суму елементів
+}
+
+Console.WriteLine($"{0} * {0}");
+
+double s = sum / (double)n;
+Console.WriteLine("Відповідь: {0}", s);
