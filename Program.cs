02994fb8f812156285ff33947058b77f193117bd
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-09-11 10:28:08 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,73 +1,27 @@
-﻿using class_car;
+﻿using shelter_homework;
 using System.Text;
 
 Console.InputEncoding = Encoding.UTF8;
 Console.OutputEncoding = Encoding.UTF8;
 
-//інформація про студента
-//клас - механізм для створення об'єктів.
-//Для створення об'єкту нам потрібно зробити клас
-//а потім на основі класу створити змінну данного класу (об'єкт)
-//string name = "Віктор";
-//short age = 18;
+Animals Мурчик = new Animals(); //створив змінну для Cat - Мурчик - об'єкт
 
-int action = 0;
-do
-{
-    Console.WriteLine("оберіть операцію");
-    Console.WriteLine("0.Вихід");
-    Console.WriteLine("1.Додати автомобіль у файл");
-    Console.WriteLine("2.Показати список автомобілів");
-    Console.Write("->");
-    action = int.Parse(Console.ReadLine());
-    switch (action)
-    {
-        case 1:
-            {
-                //new - виділяємо память під об'єкт victor
-                Car bmw = new Car();
-                Console.WriteLine("яка марка автомобіля?");
-                bmw.name = Console.ReadLine(); //змінні в середині класу називаються поля
-                Console.WriteLine("яка модель автомобіля?");
-                bmw.model = Console.ReadLine();
-                Console.WriteLine("якого року автомобіль?");
-                bmw.year = int.Parse(Console.ReadLine()); //присвоюємо значення 18
-                Console.WriteLine("розмір двигуна?");
-                bmw.engine = int.Parse(Console.ReadLine());
-                Console.WriteLine("вітаю у салон.");
-                Console.WriteLine($"{bmw.name} {bmw.model} {bmw.year} {bmw.engine}");
+Console.WriteLine("вкажіть тваринку");
+string animal = Console.ReadLine();
+Мурчик.setAnimal (animal);
+Console.WriteLine(Мурчик.getAnimal());
 
-                //Маю файл і режим роботи буде дозапису
-                //Вміст файлу видаляти не буде
-                StreamWriter sw =
-                    new StreamWriter("cars.txt", true);
-                sw.WriteLine(bmw.name);
-                sw.WriteLine(bmw.year);
-                sw.Close();
+Console.WriteLine("вкажіть ім'я тваринки");
+string name = Console.ReadLine();
+Мурчик.setName(name);
+Console.WriteLine(Мурчик.getName());
 
-                break;
-            }
-        case 2:
-            {
-                StreamReader sr = new StreamReader("cars.txt");
-                string str = sr.ReadLine();
-                while (!string.IsNullOrEmpty(str))
-                {
-                    //Console.WriteLine(str);
-                    if (!string.IsNullOrEmpty(str))
-                    {
-                        //якщо вдалося прочитати name
-                        Car temp = new Car();
-                        temp.name = str;
-                        temp.model = str;
-                        temp.year = int.Parse(sr.ReadLine()); //читаю вік
-                        Console.WriteLine($"{temp.name} -- {temp.year}");
-                    }
-                    str = sr.ReadLine();
-                }
-                str = sr.ReadLine(); //Закриваємо потік читання файлу
-                break;
-            }
-    }
+Console.WriteLine("вкажіть вік тваринки");
+int age = int.Parse(Console.ReadLine());
+Мурчик.setAge(age);
+Console.WriteLine(Мурчик.getAge());
 
-} while (action != 0);
+Console.WriteLine("вкажіть породу тваринки");
+string species = Console.ReadLine();
+Мурчик.setSpecies(species);
+Console.WriteLine(Мурчик.getSpecies());
