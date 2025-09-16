e660328bcd6a41966552cc195b27b103663b159d
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-09-16 13:18:19 +0300

    Add files via upload

---
+++ PropAndConstructor/Person.cs
@@ -0,0 +1,51 @@
+﻿namespace PropAndConstructor
+{
+    public class Person
+    {
+        private string _id = null!;
+        private string _firstName = null!;
+        private string _lastName = null!;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Phone { get; set; }
+
+        //Визивається при створенні класу
+        //Змінні набувають початково значенню
+        //Конструктор по замовчуванню
+        public Person()
+        {
+            //GUID - рандомна величина яка не повторюється
+            _id = Guid.NewGuid().ToString();
+        }
+
+        //Конструктор з параметрами
+        public Person(string lastName, string firstName, string phone): this()
+        {
+            _lastName = lastName;
+            _firstName = firstName;
+            Phone = phone;
+        }
+
+        //Повертає значення _Id
+        public string Id { get { return _id; } }
+
+        public void setFirstName(string firstName)
+        {
+            _firstName = firstName;
+        }
+        public string getFirstName()
+        {
+            return _firstName;
+        }
+
+        public override string ToString()
+        {
+            return $"{_id} -- {_firstName} {_lastName} -> {Phone}";
+        }
+    }
+}
---
+++ PropAndConstructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using PropAndConstructor;
+using System.Text;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Person victor = new Person();
+
+victor.setFirstName("Вітя");
+
+victor.LastName = "Марков"; //викликаємо set
+
+victor.Phone = "+38(097) 223 45 76";
+
+Person ira = new Person("Сидоренко", "Іра", "+38(050) 123 45 67");
+
+//Console.WriteLine(victor.getFirstName());
+//Console.WriteLine(victor.LastName); //визначаємо get
+//Console.WriteLine(victor.Phone);
+
+Console.WriteLine(victor);
+Console.WriteLine(ira);
