5a938feaf2035a695e8ab73d0aa8044400ca8d3a
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-09-18 10:37:51 +0300

    Add files via upload

---
+++ constructor_racoon/constructor_racoon/Program.cs
@@ -0,0 +1,43 @@
+﻿using constructor_racoon;
+using System;
+using System.Text;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+racoon victor = new racoon();
+
+victor.setName("Вітя");
+
+victor.name = "Марков"; //викликаємо set
+
+victor.color = "рижий";
+
+racoon pushok = new racoon("Пушок", "сірий");
+
+//Console.WriteLine(victor.getFirstName());
+//Console.WriteLine(victor.LastName); //визначаємо get
+//Console.WriteLine(victor.Phone);
+
+Console.WriteLine(victor);
+Console.WriteLine(pushok);
+
+
+
+
+capibara vitya = new capibara();
+
+vitya.setName("Вітя");
+
+vitya.name = "Вася"; //викликаємо set
+
+vitya.color = "коричневий";
+
+capibara vasya = new capibara("Гоша", "коричневий");
+
+//Console.WriteLine(victor.getFirstName());
+//Console.WriteLine(victor.LastName); //визначаємо get
+//Console.WriteLine(victor.Phone);
+
+Console.WriteLine(vitya);
+Console.WriteLine(vasya);
---
+++ constructor_racoon/constructor_racoon/racoon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor_racoon
+{
+    internal class racoon
+    {
+        private string _id = null!;
+        private string _name = null!;
+        private string _color = null!;
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string color { get; set; }
+
+        //Визивається при створенні класу
+        //Змінні набувають початково значенню
+        //Конструктор по замовчуванню
+        public racoon()
+        {
+            //GUID - рандомна величина яка не повторюється
+            _id = Guid.NewGuid().ToString();
+        }
+
+        //Конструктор з параметрами
+        public racoon(string name, string color) : this()
+        {
+            _name = name;
+            _color = color;
+        }
+
+        //Повертає значення _Id
+        public string Id { get { return _id; } }
+
+        public void setName(string name)
+        {
+            _name = name;
+        }
+        public string getName()
+        {
+            return _name;
+        }
+
+        public override string ToString()
+        {
+            return $"{_id} -- {_name} -> {_color}";
+        }
+    }
+}
