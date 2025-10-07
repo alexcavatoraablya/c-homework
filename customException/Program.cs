53f82a4e6fd8eaddac4ecd8d9f471c8505679d13
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-10-07 13:14:59 +0300

    Add files via upload

---
+++ customException/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+
+try
+{
+    Console.WriteLine("Скільки вам років");
+    Console.Write("->_");
+    string text = Console.ReadLine();
+    int age = int.Parse(text);
+
+    Console.WriteLine("Ваше ім'я");
+    Console.Write("->_");
+    int name = int.Parse(text);
+    if (age <= 0)
+    {
+        throw new Exception("Вік не може бути від'ємним числом");
+    }
+    Console.WriteLine($"ваш вік {age}");
+}
+catch (FormatException fex)
+{
+    Console.WriteLine("Дані було введено не вірно {0}", fex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Виникла помилка при введенні вашого віку {0}", ex.Message);
+}
