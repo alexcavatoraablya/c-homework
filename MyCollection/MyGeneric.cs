3416444ea2bd6ffeddd7c8a8f3d74b58a1dbd803
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-10-08 10:37:20 +0300

    Add files via upload

---
+++ MyCollection/MyGeneric.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection;
+
+public class MyGeneric
+{
+    private object[] items;
+
+    public MyGeneric()
+    {
+        items = new object[0];
+    }
+
+    public void Add(object item)
+    {
+        int n = items.Length;
+        object[] temp = new object[n+1];
+        for(int i = 0; i < n; i++)
+        {
+            temp[i] = items[i];
+        }
+        temp[n] = item;
+        items = temp;
+    }
+
+    public void ViewItems()
+    {
+        foreach (object item in items)
+        Console.WriteLine(item);
+    }
+
+    public int SearchIndex(object itemSearch)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            var item = items[i];
+            if(item.Equals(itemSearch))
+                return i;
+        }
+        //якщо не найшли
+        return -1;
+    }
+
+    public void Clear()
+    {
+        items = new object[0];
+    }
+}
---
+++ MyCollection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+//тут знаходиться набір класів для роботи з колекціями
+using System.Collections;
+using MyCollection;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+//може зберігати однаковий тип данних
+Int32[] myArray = { 23, 45, 78 };
+foreach (int i in myArray)
+{
+    Console.WriteLine("{0}\t", i);
+}
+Console.WriteLine();
+//він не є generic - він не є шаблонним класом (може зберігати будь-який тип данних)
+ArrayList myArrayList = new ArrayList();
+myArrayList.Add(23);
+myArrayList.Add("Hello");
+myArrayList.Add(true);
+myArrayList.Add(3.56);
+
+foreach (var item in myArrayList)
+{
+    Console.WriteLine("{0}\t", item);
+}
+Console.WriteLine();
+
+//CLS
+//CLR - Common Language Runtime - єдине середовище виконання
+object myObject = "hello";
+Console.WriteLine(myObject);
+
+Console.WriteLine("--------My Array Custom--------");
+
+MyGeneric victor = new MyGeneric();
+victor.Add("альона");
+victor.Add("віктор");
+victor.Add(18);
+victor.Add(22);
+
+victor.ViewItems();
+
+int index = victor.SearchIndex(18);
+Console.WriteLine("Search index = " + index);
