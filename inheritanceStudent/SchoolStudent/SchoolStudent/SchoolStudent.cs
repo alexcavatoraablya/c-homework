7e4747e2552b68227641a54d874b1a05e0f9796a
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-10-09 13:03:45 +0300

    Add files via upload

---
+++ inheritanceStudent/SchoolStudent/SchoolStudent/SchoolStudent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolStudent;
+
+internal class SchoolStudent : Person
+{
+    public SchoolStudent(string name = "Школяр") : base(name)
+    {
+
+    }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Це школяр :)");
+        base.PrintInfo();
+    }
+}
