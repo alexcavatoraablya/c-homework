a67c0d6f8837cc8a00294fc718c99e1be90eacfa
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-10-21 13:31:08 +0300

    Add files via upload

---
+++ twoArray/twoArray/MatrixCalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace twoArray;
+
+public class MatrixCalculator
+{
+    //наш масив у середині класу
+    private int[,] matrix;
+    private object myTwoArray;
+
+    public MatrixCalculator()
+    {
+        matrix = new int[2, 2];
+    }
+    public MatrixCalculator(int n, int m)
+    {
+        matrix = new int[n, m];
+        //m = matrix.GetLength(0);
+        //n = matrix.GetLength(1);
+    }
+
+    public MatrixCalculator(MatrixCalculator calc)
+    {
+        int n = calc.matrix.GetLength(0);
+        int m = calc.matrix.GetLength(1);
+        matrix = new int[n, m];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = calc.matrix[i, j];
+            }
+        }
+    }
+
+    public void intRandomMatrix(int min=1, int max=100)
+    {
+        Random random = new Random();
+        int n = matrix.GetLength(0);
+        int m = matrix.GetLength(1);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = random.Next(1, 100);
+            }
+        }
+    }
+
+    public void PrintMatrix()
+    {
+        int n = matrix.GetLength(0);
+        int m = matrix.GetLength(1);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ twoArray/twoArray/Program.cs
@@ -0,0 +1,50 @@
+﻿using twoArray;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+//int[] myArray = { 3, 8, 12, 42 };
+//foreach (var item in myArray)
+//{
+//    Console.Write(item + "\t");
+//}
+//Console.WriteLine();
+
+//int[,] myTwoArray =
+//{
+//    {9, 45, 12 },
+//    {42, 89, 90 },
+//    {3, 78, 6 }
+//};
+
+//int n = myTwoArray.GetLength(0); //рядки
+//int m = myTwoArray.GetLength(1); //стовпці
+
+////myTwoArray[0, 0] \t myTwoArray[0,1] \t myTwoArray[0,2] \n
+
+//for (int i = 0; i < n; i++)
+//{
+//    for (int j = 0; j < m; j++)
+//    {
+//        Console.Write(myTwoArray[i, j] + "\t");
+//    }
+//    Console.WriteLine();
+//}
+
+/*
+MatrixCalculator calculator = new MatrixCalculator();
+//MatrixCalculator calculator = new MatrixCalculator(2,4);
+
+//заповняємо матрицю числами від 1 до 100
+calculator.intRandomMatrix(1, 100);
+calculator.PrintMatrix();
+
+*/
+
+MatrixCalculator a = new MatrixCalculator(3, 4);
+
+a.intRandomMatrix();
+
+MatrixCalculator b = new MatrixCalculator(a);
+
+b.PrintMatrix();
