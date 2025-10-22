0e48d13ba17ad320c64a1737b6b963563e836c24
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-10-22 10:19:18 +0300

    Add files via upload

---
+++ ConsoleMatrix/ConsoleMatrix/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleMatrix;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+MyMatrix a = new MyMatrix();
+a.InitRandom();
+
+//Console.WriteLine("a[0,0] = "+ a[0,0]);
+Console.WriteLine("--------наша матриця A--------");
+Console.WriteLine(a.ToString());
+
+MyMatrix b = new MyMatrix();
+b.InitRandom();
+Console.WriteLine("--------наша матриця B--------");
+Console.WriteLine(b.ToString());
+
+MyMatrix c = a.Add(b);
+Console.WriteLine("--------наша матриця A + B--------");
+Console.WriteLine(c.ToString());
+
+MyMatrix d = a.Minus(b);
+Console.WriteLine("--------наша матриця A - B--------");
+Console.WriteLine(d.ToString());
+
+MyMatrix e = a.Multiply(b);
+Console.WriteLine("--------наша матриця A * B--------");
+Console.WriteLine(e.ToString());
