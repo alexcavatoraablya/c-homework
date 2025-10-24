7a611e75cf2b24cb8e6acc58eae73eb2d156bef7
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-10-24 08:40:58 +0300

    Add files via upload

--- exceptionHW/exceptionHW/Program.cs
+++ exceptionHW/exceptionHW/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using exceptionHW;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+try
+{
+    Console.WriteLine("Введіть назву файлу: ");
+    Console.Write("->_");
+    string fileName = Console.ReadLine();
+
+    Reader reader = new Reader(fileName);
+    reader.ReadFile();
+}
+catch (FileNotFoundException fnfex)
+{
+    Console.WriteLine("Файл не знайдено. {0}", fnfex.Message);
+}
+catch (UnauthorizedAccessException uex)
+{
+    Console.WriteLine("Файл неможна прочитати. {0}", uex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Помилка. {0}", ex.Message);
+}
--- exceptionHW/exceptionHW/Reader.cs
+++ exceptionHW/exceptionHW/Reader.cs
@@ -4,9 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace exceptionHW
+namespace exceptionHW;
+
+public class Reader
 {
-    internal class Reader
-    {
-    }
+        private string _fileName;
+
+        public Reader(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public void ReadFile()
+        {
+            // Перевірка чи існує файл
+            if (!File.Exists(_fileName))
+            {
+                throw new FileNotFoundException("Такого файлу не існує.", _fileName);
+            }
+
+            try
+            {
+                // Перевірка чи можна відкрити файл для читання
+                using (FileStream fs = File.Open(_fileName, FileMode.Open, FileAccess.Read))
+                {
+
+                }
+
+            }
+            // Якщо немає доступу до файлу
+            catch (UnauthorizedAccessException uex)
+            {
+                // Перевикидаємо виняток вище
+                throw new UnauthorizedAccessException("Файл неможна прочитати.", uex);
+            }
+
+            // Читаємо всі рядки з файлу
+            string[] lines = File.ReadAllLines(_fileName);
+
+            // Перевіряємо чи більше 100 рядків
+            if (lines.Length > 100)
+            {
+                // Перевикидаємо виняток вище
+                throw new Exception("У файлі більше 100 рядків!");
+            }
+            // Виводимо вміст файлу
+            Console.WriteLine("Вміст файлу: ");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
 }
