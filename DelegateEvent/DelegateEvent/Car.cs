6fe580416c3a94ff8a4386ba9f960017973a365c
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-10-29 10:25:40 +0200

    Add files via upload

---
+++ DelegateEvent/DelegateEvent/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEvent;
+
+public class Car
+{
+    //данний делегат повідомляє інформацію про автомобіль
+    public delegate void DisplayInfoDelegate(string info);
+    public delegate void DisplayProblemInfoDelegate(string info);
+    //поле для зберігання посилання на метод делегата
+    //повідомляє на дисплей автомобіля інформацію
+    private DisplayInfoDelegate? displayInfoCallback;
+    //повідомляє на дисплей автомобіля інформацію про проблеми
+    private DisplayProblemInfoDelegate? displayProblemInfoCallback;
+
+    private string? model; //може зберігати null
+    private int speed;
+    private int maxSpeed;
+
+    public Car()
+    {
+        maxSpeed = 200;
+    }
+    public Car(string? model, int maxSpeed)
+    {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        speed = 0;
+    }
+
+    public void RegisterDisplayAuto(DisplayInfoDelegate? displayInfoCallback)
+    {
+        //реєструємо метод делегата (у авто є дисплей)
+        this.displayInfoCallback = displayInfoCallback;
+    }
+
+    public void RegisterDisplayProblemAuto(DisplayProblemInfoDelegate? displayProblemInfoCallback)
+    {
+        //реєструємо метод делегата для повідомлення про проблеми
+        this.displayProblemInfoCallback = displayProblemInfoCallback;
+    }
+
+    public void Run(int speed)
+    {
+        this.speed += speed;
+        if (this.speed > 0)
+        {
+            if(displayInfoCallback != null)
+            {
+                displayInfoCallback($"Автомобіль {model} розганяється до швидкості {this.speed} км/год");
+            }
+        }
+
+        if(this.speed >= maxSpeed)
+        {
+            if (displayProblemInfoCallback != null)
+            {
+                displayProblemInfoCallback($"Автомобіль {model} досяг максимальної швидкості {maxSpeed} км/год і зупиняється!");
+            }
+            speed = maxSpeed;
+        }
+    }
+
+
+    private int Speed
+    {
+        get { return speed; }
+    }
+    private string? Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+}
---
+++ DelegateEvent/DelegateEvent/Program.cs
@@ -0,0 +1,24 @@
+﻿using DelegateEvent;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+void DisplayAutoInfo(string info) //дисплей авто
+{
+    Console.WriteLine(info);
+}
+
+void DisplayAutoErrorInfo(string info) //дисплей авто
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("-----------------");
+    Console.WriteLine(info);
+    Console.WriteLine("-----------------");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+//створюємо об'єкт автомобіля
+Car car = new Car("BMW", 240);
+car.RegisterDisplayAuto(DisplayAutoInfo);//підключили дисплей для авто
+car.RegisterDisplayProblemAuto(DisplayAutoErrorInfo);//підключили дисплей для проблем авто
+car.Run(50);
+car.Run(200); //збільшуємо швидкість на 200 км/год
