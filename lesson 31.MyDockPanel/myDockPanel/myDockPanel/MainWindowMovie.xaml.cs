4afb91dbcd7221f0f211d65922c00877ced34228
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-11-18 13:18:04 +0200

    Add files via upload

---
+++ lesson 31.MyDockPanel/myDockPanel/myDockPanel/MainWindowMovie.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowMovie()
+        {
+            InitializeComponent();
+            var movie = new Movie
+            {
+                Title = "The Pink Panther",
+                Year = "1963",
+                Description = "Фільм про розслідування Жака Клузо.",
+                PosterUrl = "images/posterPP.jpg",
+                Rating = 7
+            };
+            var win = new myDockPanel(movie);
+            win.Show();
+
+            DataContext = movie;
+        }
+    }
+}
---
+++ lesson 31.MyDockPanel/myDockPanel/myDockPanel/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainWindow;
+
+public class Movie
+{
+    public string Title { get; set; }
+    public string Year { get; set; }
+    public string Description { get; set; }
+    public string PosterUrl { get; set; }
+    public double Rating { get; set; }
+}
