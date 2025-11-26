fa1663bbfb9cb7ade0a1325d99e2d399ee337c26
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-11-26 10:18:47 +0200

    Add files via upload

---
+++ WpfWorkingWindow/WpfWorkingWindow/City/CityAddWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfWorkingWindow.Models;
+
+namespace WpfWorkingWindow.City
+{
+    /// <summary>
+    /// Interaction logic for CityAddWindow.xaml
+    /// </summary>
+    public partial class CityAddWindow : Window
+    {
+        public CityAddWindow()
+        {
+            InitializeComponent();
+        }
+        public CityModel CityItem { get; set; }
+
+        internal object ToList()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnAddCity_Click(object sender, RoutedEventArgs e)
+        {
+            CityModel newCity = new CityModel
+            {
+                Name = txtCityName.Text,
+                Image = txtCityImage.Text,
+                Population = int.Parse(txtCityPopulation.Text),
+                Area = double.Parse(txtCityArea.Text)
+            };
+            CityItem = newCity;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ WpfWorkingWindow/WpfWorkingWindow/Models/CityModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfWorkingWindow.Models;
+
+public class CityModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public int Population { get; set; }
+    public double Area { get; set; }
+}
